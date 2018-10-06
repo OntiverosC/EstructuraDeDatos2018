00988fc9c79839dab7aa78310f37a76ec5762a21
OntiverosC <42825574+OntiverosC@users.noreply.github.com>
2018-10-06 14:29:20 -0700

    Delete Ejercicio1Examen.py

--- Ejercicio1Examen.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# Ontiveros Lara Claudia Sarahi.
-def Sumatoria(n):                  
-  if n==1:                                     
-    return 1         
-  else:
-      return Sumatoria(n-1)+n 
-print(Sumatoria(9)) 
